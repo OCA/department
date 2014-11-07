c440e37399e5399d9a7ac56ff472d188ef923211
Daniel Reis <dgreis@sapo.pt>
2014-11-07 11:43:34 +0000

    Remove redundancies

--- invoice_department/__openerp__.py
+++ invoice_department/__openerp__.py
@@ -23,7 +23,7 @@
     'name': 'Invoices with Department Categorization',
     'version': '1.0',
     'category': 'Generic Modules/Sales & Purchases',
-    'description': '''\
+    'description': '''
 Add the department on Invoices as well as the related filter and button in the
 search form.
 ''',
@@ -31,9 +31,5 @@ search form.
     'website': 'http://camptocamp.com',
     'depends': ['account', 'hr'],
     'data': ['invoice_view.xml'],
-    'demo': [],
-    'test': [],
     'installable': True,
-    'auto_install': False,
-    'application': False
 }
