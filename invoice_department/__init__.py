ab75a712f95b9574d961cf0da39e6a1b10657899
Luis Felipe Mileo <mileo@kmee.com.br>
2016-06-20 07:48:27 -0300

    Clear travis errors

--- invoice_department/__init__.py
+++ invoice_department/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011 Camptocamp SA (http://www.camptocamp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import models
 from . import report
