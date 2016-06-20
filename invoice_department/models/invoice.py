0a00e3965838b3d72fc1206bd2d10816d43e1174
Luis Felipe Mileo <mileo@kmee.com.br>
2016-06-20 07:46:13 -0300

    [FIX] get default department

--- invoice_department/models/invoice.py
+++ invoice_department/models/invoice.py
@@ -8,17 +8,17 @@ from openerp import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
+    @api.model
+    def _get_default_department(self):
+        department_id = self.env['hr.department']
+        for employee in self.env.user.employee_ids:
+            if employee and employee.department_id:
+                department_id = employee.department_id
+                break
+        return department_id
+
     department_id = fields.Many2one(
         comodel_name='hr.department',
         string='Department',
-        default='_get_department',
+        default=_get_default_department,
     )
-
-    @api.model
-    def _get_department(self):
-        department_id = False
-        for employee in self.env.user.employee_ids:
-            if employee and employee.department_id:
-                department_id = employee.department_id.id
-                break
-        return department_id
